deac304a7facd2fb311b7de75e408a5bffb96702
pinoy ninja coder <39272230+pncoder29@users.noreply.github.com>
2024-05-02 11:08:27 +0800

    done CRUD
    This function added features CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #add task
     path("addTask/", views.addTask, name="addTask"),
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name="mark_as_done")
+    
+    #this for button mark as done going to complete task
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name="mark_as_done"),
+    
+    #this for mark undone task
+    # path('mark_as_undone/<int:pk>/', views.mark_as_undone, name="mark_as_undone"),
+    
+    #this for Edit features
+    path("edit_task/<int:pk>/", views.edit_task, name="edit_task"),
+    
+    #this is for Delete task
+    path("delete_task/<int:pk>/", views.delete_task, name="delete_task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -8,9 +8,59 @@ def addTask(request):
     task.objects.create(task=task01)
     return redirect("home")
 
+#this for button of mark as done
 def mark_as_done(request, pk):
     task02 = get_object_or_404(task, pk=pk)
     task02.is_completed = True 
     task02.save()
     return redirect("home")
+
+
+#this for Edit task Features
+def edit_task(request, pk):
+    get_task = get_object_or_404(task, pk=pk)
+    if request.method == "POST":
+        #targeting the <input type name"task">
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect("home")
+    else:
+        context ={
+            "get_task":get_task,
+        }
+    return render(request, "edit_task.html", context)
+
+#This for delete task features
+def delete_task(request, pk):
+    task02 = get_object_or_404(task, pk=pk)
+    task02.delete()
+    return redirect("home")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
